a6a3d87c0993a3a5bec46574ae6e9616034d00b1
shetara2022 <smithshetarall@gmail.com>
2022-12-27 18:29:30 -0500

    parse local storage, created a score object to push scores to highscores array

--- saveScore.js
+++ saveScore.js
@@ -4,6 +4,11 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore')     //get finalscore id from html
 const mostRecentScore = localStorage.getItem('mostRecentScore')   //get recent score from local storage
 
+//LOCAL STORAGE-things in local storage are stored as a JSON string 
+//parse to get an array object out of local storage
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];   //initializes empty highscore array
+console.log(highScores)
+
 finalScore.innerText = mostRecentScore    //updates final score with the most recent score
 
 //add eventlistener using 'keyup'-gives change on every character
@@ -14,4 +19,14 @@ username.addEventListener('keyup', () => {
 saveHighScore = (e) => {
     // console.log("clicked save");
     e.preventDefault();
+
+    //Score object 
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    //push score to array
+    highScores.push(score)
+    console.log(highScores);
 };
+
