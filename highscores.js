3f3e072cd2a84a1ba3844af94011f4bbd0e61ccb
shetara2022 <smithshetarall@gmail.com>
2022-12-27 19:33:53 -0500

    styled highscore page

--- highscores.js
+++ highscores.js
@@ -5,6 +5,6 @@ const highScores = JSON.parse(localStorage.getItem('highScores')) || [];  //get 
 //iterate through each score and add to list 
 highScoresList.innerHTML =
     highScores.map(score => {     //taking in the score object & returning a string version of a list
-        return `<li class ="high-score">${score.name} -${score.score} </li>`
+        return `<li class ="high-score">${score.name} - ${score.score} </li>`
     })
         .join('');
