f0c0683b4c41f16f96db408eb4f39ea3b47cfc09
shetara2022 <smithshetarall@gmail.com>
2022-12-27 19:22:31 -0500

    created highscore display page, pulled data from local storage and map through data to display as list on the page

---
+++ highscores.js
@@ -0,0 +1,10 @@
+const highScoresList = document.getElementById('highScoresList');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];  //get high scores out of local storage [copied from saveScore.js]
+
+
+//iterate through each score and add to list 
+highScoresList.innerHTML =
+    highScores.map(score => {     //taking in the score object & returning a string version of a list
+        return `<li class ="high-score">${score.name} -${score.score} </li>`
+    })
+        .join('');
